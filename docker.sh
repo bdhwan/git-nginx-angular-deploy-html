02b5b9a23c9c346cc4f5f55671dd853d20ce1340
Robert Bae <bdhwan@gmail.com>
2020-05-31 00:24:40 +0900

    fix

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build  --no-cache --tag bdhwan/git-nginx-angular-deploy-html:0.4 .
+docker build --tag bdhwan/git-nginx-angular-deploy-html:0.4 .
 docker push bdhwan/git-nginx-angular-deploy-html:0.4
